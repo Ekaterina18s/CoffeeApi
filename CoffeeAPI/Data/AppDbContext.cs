8f4d855d94fafae9ae59af5a25dad27285d2e736
Ekaterina18s <ekaterinaelzo18@gmail.com>
2024-04-01 15:42:06 +0300

    Add project files.

---
+++ CoffeeAPI/Data/AppDbContext.cs
@@ -0,0 +1,5 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+}
---
+++ CoffeeAPI/Data/Entities/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿ namespace CoffeeAPI.Data.Entities
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+    }
+}
